0919724162a4644e2b83e499675e5ee2f90f7919
Kingsley <k@n.com>
2020-02-27 12:03:40 +0100

    Removed answer

--- js/master.js
+++ js/master.js
@@ -7,24 +7,9 @@ fetch("https://api.punkapi.com/v2/beers")
 function handleApiJson(json) {
 
     // filter out beers that are less than 5% abv
-    const strongBeers = json.filter(beer => beer.abv >= 5);
-
     
     // convert data to HTML
-    const beersHTML = strongBeers.map(beer => `
-        <article class="beer">
-            <h2 class="beer__name">${beer.name}</h2>
-            <h3 class="beer__tagline">${beer.tagline}</h3>
-
-            <img class="beer__img" src="${beer.image_url}" alt="${beer.name} image" />
-            
-            <p class="beer__description">${beer.description}</p>
-
-            <div class="beer__abv">${beer.abv}%</div>
-        </article>
-    `).join(""); // make sure to join("") the mapped array
-
+    // make sure to join("") the mapped array
 
     // select element and update the innerHTML with what we have created
-    document.getElementById("beers").innerHTML = beersHTML;
 }
